13013f15db9fdab8733474e536ae9011ff735d00
Aisen Kim <aisenkim15@gmail.com>
2021-07-20 18:19:27 +0900

    changed home page to see retests for each user

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,77 +1,104 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import { Container, Jumbotron, Row, Col } from 'react-bootstrap'
+import {Container, Jumbotron, Row, Col} from 'react-bootstrap'
 import isAuthenticated from '../functions/authService'
 
 function Home(props) {
-  // const [from, setFrom] = useState("");
-  // const [to, setTo] = useState("");
+    // const [from, setFrom] = useState("");
+    // const [to, setTo] = useState("");
+    const [retests, setRetests] = useState([]);
 
-  const userLevel = localStorage.getItem('level')
-  const token = localStorage.getItem('token')
+    const userLevel = localStorage.getItem('level')
+    const token = localStorage.getItem('token')
 
-  // GET today's date
-  const today = new Date()
-  const dd = String(today.getDate()).padStart(2, '0')
-  const mm = String(today.getMonth() + 1).padStart(2, '0')
-  const yyyy = today.getFullYear()
-  const testDate = yyyy + '-' + mm + '-' + dd
+    // GET today's date
+    const today = new Date()
+    const dd = String(today.getDate()).padStart(2, '0')
+    const mm = String(today.getMonth() + 1).padStart(2, '0')
+    const yyyy = today.getFullYear()
+    const testDate = yyyy + '-' + mm + '-' + dd
 
-  // useEffect(() => {
-  //   async function getPlan() {
-  //     try {
-  //       const plan = await axios.get(
-  //         `plan/getPlan?level=${userLevel}&testDate=${testDate}`,
-  //         { headers: { Authorization: `Bearer ${token}` } },
-  //       )
-  //       // extract "from" and "to" from plan
-  //       console.log(plan)
-  //       const planData = plan.data[0]
-  //       const { from, to } = planData
-  //       // setFrom(from);
-  //       // setTo(to);
-  //       // make request to get test data
-  //     } catch (error) {
-  //       if (!isAuthenticated()) {
-  //         props.setAppToken('')
-  //         props.setAppUser('')
-  //       }
-  //       console.log(error)
-  //     }
-  //   }
-  //   getPlan()
-  // }, [])
+    useEffect(() => {
+        async function getRetest() {
+            try {
+                const retest = await axios.get('retests', {
+                    headers: {Authorization: `Bearer ${token}`},
+                })
+                console.log(retest.data)
+                setRetests(retest.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
 
-  // axios
-  //   .get(`testing/getPlan?level=${userLevel}&testDate=${testDate}`, {
-  //     headers: { Authorization: `Bearer ${token}` },
-  //   })
-  //   .then((plan) => {
-  //     const planData = plan.data;
-  //     const from = planData[0].from;
-  //     const to = planData[0].to;
+        getRetest()
+    }, []);
 
-  //   });
 
-  return (
-    <Container className="mt-4" fluid>
-      <Row>
-        <Col md={{ span: 3, offset: 4 }}>
-          <h1>Welcome {props.appUser}!</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={{ span: 2, offset: 4 }}>
-          <h3>오늘 할일</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={{ span: 2, offset: 4 }}>
-          {props.appUser && userLevel === 'sp3' ? <h1>Test exist</h1> : null}
-        </Col>
-      </Row>
-    </Container>
-  )
+    // useEffect(() => {
+    //   async function getPlan() {
+    //     try {
+    //       const plan = await axios.get(
+    //         `plan/getPlan?level=${userLevel}&testDate=${testDate}`,
+    //         { headers: { Authorization: `Bearer ${token}` } },
+    //       )
+    //       // extract "from" and "to" from plan
+    //       console.log(plan)
+    //       const planData = plan.data[0]
+    //       const { from, to } = planData
+    //       // setFrom(from);
+    //       // setTo(to);
+    //       // make request to get test data
+    //     } catch (error) {
+    //       if (!isAuthenticated()) {
+    //         props.setAppToken('')
+    //         props.setAppUser('')
+    //       }
+    //       console.log(error)
+    //     }
+    //   }
+    //   getPlan()
+    // }, [])
+
+    // axios
+    //   .get(`testing/getPlan?level=${userLevel}&testDate=${testDate}`, {
+    //     headers: { Authorization: `Bearer ${token}` },
+    //   })
+    //   .then((plan) => {
+    //     const planData = plan.data;
+    //     const from = planData[0].from;
+    //     const to = planData[0].to;
+
+    //   });
+
+    return (
+        <Container className="mt-4" fluid>
+            <Row>
+                <Col md={{span: 3, offset: 4}}>
+                    <h1>Welcome {props.appUser}!</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={{span: 2, offset: 4}}>
+                    <h3>My Retest</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={{span: 2, offset: 4}}>
+                    {props.appUser && userLevel === 'sp3' ? <h1>Test exist</h1> : null}
+                </Col>
+            </Row>
+            {retests.map((retest, idx) => {
+                return !retest ? null : (
+                    <Row key={idx}>
+                        <Col md={{span: 2, offset: 4}}>
+                            <p>{retest.testDate}</p>
+                        </Col>
+                    </Row>
+                )
+            })}
+        </Container>
+    )
 }
 
 export default Home
