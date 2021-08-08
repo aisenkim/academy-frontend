b68dd3c1288b8167fbb0e54cb40e4542b18972d5
Aisen Kim <aisenkim15@gmail.com>
2021-08-09 03:06:41 +0900

    changed from BrowserRouter to HashRouter because getting 404 on hitting routes in github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Switch, Route, HashRouter} from 'react-router-dom'
 import StartTest from './routes/StartTest'
 import Home from './routes/Home'
 import Navigation from './components/Navigation'
@@ -14,80 +14,80 @@ import Retest from "./routes/Retest";
 import CreateUser from "./routes/CreateUser";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'))
-  const [user, setUser] = useState(localStorage.getItem('name'))
+    const [token, setToken] = useState(localStorage.getItem('token'))
+    const [user, setUser] = useState(localStorage.getItem('name'))
 
-  return (
-    <BrowserRouter>
-      <Navigation
-        appToken={token}
-        setAppToken={setToken}
-        appUser={user}
-        setAppUser={setUser}
-      />
-      <Switch>
-        <Route exact path="/startTest" component={StartTest} />
-        <Route exact path="/signin">
-          <Login setAppUser={setUser} />
-        </Route>
-        <ProtectedRoute
-          exact
-          path="/createPlan"
-          component={Plan}
-          setAppToken={setToken}
-          setAppUser={setUser}
-          appUser={user}
-        />
-        <ProtectedRoute
-          exact
-          path="/user-scores/person"
-          component={UserScorePerson}
-          setAppToken={setToken}
-          setAppUser={setUser}
-        />
-        <ProtectedRoute
-            path="/retests"
-            component={Retest}
-            setAppToken={setToken}
-            setAppUser={setUser}
-        />
-        <ProtectedRoute
-          exact
-          path="/user-scores/person/questions"
-          component={UserTestQuestion}
-          setAppToken={setToken}
-          setAppUser={setUser}
-        />
-        <ProtectedRoute
-          exact
-          path="/user-scores"
-          component={UserScore}
-          setAppToken={setToken}
-          setAppUser={setUser}
-          appUser={user}
-        />
-        <ProtectedRoute
-          exact
-          path="/users"
-          component={CreateUser}
-          setAppToken={setToken}
-          setAppUser={setUser}
-        />
-        <Route
-          exact
-          path="/createTest"
-          component={CreateTest}
-          setAppToken={setToken}
-          setAppUser={setUser}
-          appUser={user}
-        />
-        {/* <Route exact path="/createPlan" component={Plan} /> */}
-        <Route path="/" exact={true}>
-          <Home appUser={user} setAppToken={setToken} setAppUser={setUser} />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  )
+    return (
+        <HashRouter>
+            <Navigation
+                appToken={token}
+                setAppToken={setToken}
+                appUser={user}
+                setAppUser={setUser}
+            />
+            <Switch>
+                <Route exact path="/startTest" component={StartTest}/>
+                <Route exact path="/signin">
+                    <Login setAppUser={setUser}/>
+                </Route>
+                <ProtectedRoute
+                    exact
+                    path="/createPlan"
+                    component={Plan}
+                    setAppToken={setToken}
+                    setAppUser={setUser}
+                    appUser={user}
+                />
+                <ProtectedRoute
+                    exact
+                    path="/user-scores/person"
+                    component={UserScorePerson}
+                    setAppToken={setToken}
+                    setAppUser={setUser}
+                />
+                <ProtectedRoute
+                    path="/retests"
+                    component={Retest}
+                    setAppToken={setToken}
+                    setAppUser={setUser}
+                />
+                <ProtectedRoute
+                    exact
+                    path="/user-scores/person/questions"
+                    component={UserTestQuestion}
+                    setAppToken={setToken}
+                    setAppUser={setUser}
+                />
+                <ProtectedRoute
+                    exact
+                    path="/user-scores"
+                    component={UserScore}
+                    setAppToken={setToken}
+                    setAppUser={setUser}
+                    appUser={user}
+                />
+                <ProtectedRoute
+                    exact
+                    path="/users"
+                    component={CreateUser}
+                    setAppToken={setToken}
+                    setAppUser={setUser}
+                />
+                <Route
+                    exact
+                    path="/createTest"
+                    component={CreateTest}
+                    setAppToken={setToken}
+                    setAppUser={setUser}
+                    appUser={user}
+                />
+                {/* <Route exact path="/createPlan" component={Plan} /> */}
+                <Route path="/" exact={true}>
+                    <Home appUser={user} setAppToken={setToken} setAppUser={setUser}/>
+                </Route>
+            </Switch>
+        </HashRouter>
+    )
 }
 
 export default App
