a3bdab673ab8a7aa65127c3da9d3924d4b019448
Aisen Kim <aisenkim15@gmail.com>
2021-08-09 02:33:45 +0900

    ready to deploy

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
-        axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
